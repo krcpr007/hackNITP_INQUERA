36f4929e9bc946fb7b1cff402748045878386fc7
Ayushi Gupta <76425783+Ayushi1881@users.noreply.github.com>
2022-02-12 05:02:06 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,15 +13,15 @@ function Login() {
     }
 }, [])
   return (
-    <div className="hero min-h-screen ">
+    <div className="hero min-h-screen bg-[#0e1e2c]">
       <div className="flex-col justify-center hero-content lg:flex-row">
-        <div className="text-center lg:text-left md:px-20">
-          <h1 className="mb-5 text-5xl font-bold text-indigo-700">Wellcome Back!</h1>
-          <p className="mb-5">
-          Thanks for revisiting InQuera...
+        <div className="text-center lg:text-left  md:px-20 mx-11">
+          <h1 className="mb-5 text-5xl font-bold text-[#FFE400] text-center">Welcome to Inquera</h1>
+          <p className="mb-5 text-center">
+          bcoz every question has an answer...
           </p>
         </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-slate-800">
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#061229]">
           <form>
             <div className="card-body">
               <div className="form-control">
@@ -30,7 +30,7 @@ function Login() {
                 </label>
                 <input
                   placeholder="email"
-                  className="input input-bordered"
+                  className="input input-warning input-bordered bg-[#0e1e2c]"
                   type="email"
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
@@ -42,16 +42,16 @@ function Login() {
                 </label>
                 <input
                   placeholder="password"
-                  className="input input-bordered"
+                  className="input input-warning input-bordered bg-[#0e1e2c]"
                   type="password"
                   onChange={(e) => setPassword(e.target.value)}
                   value={password}
                 />
-                <label className="label text-indigo-700 text-sm">
-                  <a href="/" className="">
+                <label className="label text-sky-600 text-sm ">
+                  <a href="/" className="hover:text-yellow-200">
                     Forgot password?
                   </a>
-                  <p className=" ">
+                  <p className="hover:text-yellow-200 ">
                     <Link to="/signup">Signup</Link>
                   </p>
                 </label>
@@ -61,7 +61,7 @@ function Login() {
                   type="submit"
                   value="Login"
                   onClick={handleLogin}
-                  className="btn btn-secondary"
+                  className="btn btn glass bg-[#04293A] active:loading"
                 >
                   Login
                 </button>
