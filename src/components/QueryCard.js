9be9dd7df8cb56bc14f3c652a40c0b5b01c601a7
Ayushi Gupta <76425783+Ayushi1881@users.noreply.github.com>
2022-02-12 13:23:17 +0530

    Update QueryCard.js

--- src/components/QueryCard.js
+++ src/components/QueryCard.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 function QueryCard() {
   return (
-    <div>QueryCard</div>
+    <div class="card lg:card-side card-bordered text m-11 bg-[#577BC1]">
+  <div class="flex items-center w-full px-4 py-10 bg-cover card bg-base-200">
+  <div class="card glass lg:card-side text-neutral-content">
+    <div class="w-fit card-body">
+      <h2 class="card-title text-stone-900 font-extrabold text-2xl">Will IIT/NITs ever open?</h2> 
+      <p className='text-[#FFE400] font-extrabold'>USERNAME </p>
+      <p>Many private colleges have reopened in DECEMBER, but for government colleges the prospects of reopening before April 2021 look dull.
+
+      IIT ROPAR has informed that upto April 2021 we will monitor the situation and take a call only if the situation abates, NIT Jalandhar has decided to go online for the next semester and so is the case with many other NITs/IITs/IIITs.
+
+      IITs are having most of the Phd students back on the campus but reopening for UG students is a tough nut because they usually share hostel rooms and live in close network unlike the Phd students.
+
+      Maybe NITs/IITs/IIITs take cue from the private colleges and reopen the campus. Lets hope for the best😃. What do you think? comment down below.</p> 
+      <div class="card-actions">
+        <button class="bg-transparent hover:bg-blue-900 text-blue-800 font-semibold hover:text-white py-2 px-4 border border-blue-800 hover:border-transparent rounded">Upvote 💙</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
   )
 }
 
