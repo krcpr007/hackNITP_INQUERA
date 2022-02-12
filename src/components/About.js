a1e0cc62ab4f747a079febf582621753a7bf011d
Rajan kumar <krcpr007@gmail.com>
2022-02-12 17:04:42 +0530

    About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,18 @@
 import React from "react";
-
+import logo from './logo.png'
 function About() {
   return (
   <> 
-    <div>
+    <div className="bg-[#0e1e2c]">
         <div className="p-5 md:px-20">
-        <h1 className=" text-5xl text-center font-medium text-yellow-500">Welcome to InQuera </h1>
+        <h1 className=" text-5xl text-center font-medium font-serif text-yellow-500 underline">InQuera </h1>
         <div>
-            <h1 className="text-2xl font-bold">About</h1>
+            <h1 className="text-2xl font-bold text-rose-500">About</h1>
             <hr />
             <p className="p-5 text-justify font-semibold">Inquera is an online asking platform where anyone comes and asks their own queries and get answers from other users of inquera also user can answer the question or query of other users. </p>
         </div>
         <div>
-            <h1 className="text-2xl font-bold">Vision</h1>
+            <h1 className="text-2xl font-bold text-rose-500">Vision</h1>
             <hr />
             <p className="p-5 text-justify font-medium">Inquera mission is to share and grow the world’s knowledge. As we all knowledge cant be written down, but much of that which can be still isn't. It remains in people’s heads or is only accessible if you know the right people. We want to connect the people who have the knowledge to the people who need it, to bring together people with different perspectives so they can understand each other better, and to empower everyone to share their knowledge for the benefit of the rest of the world.
             <br />
@@ -20,9 +20,9 @@ function About() {
             </p>
         </div>
         <div>
-            <h1 className="text-2xl font-bold">Creaters</h1>
+            <h1 className="text-2xl font-bold text-rose-500">Creaters</h1>
             <hr />
-            <h1 className="text-center">Team Wizard</h1>
+            <h1 className="text-center text-3xl text-yellow-400">Team Wizard</h1>
             <ul>
               <li>
                 Ayushi Gupta
