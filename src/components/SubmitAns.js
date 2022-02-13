6b11fb53145fac5adf03a8501964e2582d5179bf
Rajan kumar <krcpr007@gmail.com>
2022-02-13 11:57:35 +0530

    link changes and pic added

--- src/components/SubmitAns.js
+++ src/components/SubmitAns.js
@@ -61,19 +61,18 @@ function SubmitAns() {
         <QueryCard title={data.title} name={data.name} query={data.query} tags={data.tags} name={data.name} email={data.email} _id={data._id} />
         <div className='px-3 md:px-20'>
         {ansData.map((item)=>{
-          return <AnswerCard key={item._id} answer={item.answer} email={item.email} name={item.name} likes={item.likes} />
+          return <AnswerCard key={item._id} id={item._id} answer={item.answer} email={item.email} name={item.name} likes={item.likes} />
         })}
 
         </div>
             <div className='container px-10 md:px-40  md:py-10'>
               <form>
-                {/* <textarea className='justify-center rounded-lg input border-none border-b-2 border-yellow-300 h-20 w-full bg-slate-800' onChange={(e)=>setAnswer(e.target.value)}/> */}
-                <div class="form-control my-1">
-                <label class="label">
+                <div className="form-control my-1">
+                <label className="label">
                 </label> 
-                <label class="input-group input-group-vertical input-group-lg">
+                <label className="input-group input-group-vertical input-group-lg">
                   <span className='bg-slate-900'>Write your answer</span> 
-                  <textarea type="text" placeholder="Type Your answer" class="input input-bordered input-lg w-full h-24" onChange={(e)=>setAnswer(e.target.value)}/>
+                  <textarea type="text" placeholder="Type Your answer" className="input input-bordered input-lg w-full h-24" onChange={(e)=>setAnswer(e.target.value)}/>
                 </label>
               </div>
                 <button type='submit' className='btn btn glass bg-[#04293A]' value={answer} onClick={submitAnswer}> Submit</button>
