0c7a0172e239f2cb2008cf749e39f7b3a536ee4e
Rajan kumar <krcpr007@gmail.com>
2023-02-06 10:56:50 +0530

    code refactor

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,16 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { toast } from 'react-toastify';
 function ContactUs() {
     const auth = localStorage.getItem('inquera-user');
     const profileData = JSON.parse(auth);
-    const [name, setName] = useState(profileData.name);
-    const [email, setEmail] = useState(profileData.email);
+    // console.log(profileData)
+    // const [name, setName] = useState(profileData.name);
+    // const [email, setEmail] = useState(profileData.email);
     const [msg, setMsg] = useState('');
-    useEffect(() => {
-        toast.error("Login Firstly");
-        setName("");
-        setEmail("");
-    }, [])
     const handleONSubmit = () => {
         if (msg.length <= 20) {
             toast.error("Please Enter details");
@@ -20,18 +16,17 @@ function ContactUs() {
     return (
         <>
             <div className='py-8 bg-[#0e1e2c]'>
-                <h1 className='text-5xl text-center'>Contact us</h1>
-                <div className="bg-slate-800 shadow-2xl rounded-md mx-20 py-4">
-                    <h1 className='text-center text-xl font-medium md:text-3xl py-1 text-rose-400'>Post your suggestions and complaints</h1>
+                <div className="bg-slate-800 shadow-2xl rounded-md mx-20 py-4 border border-yellow-400">
+                    <h1 className='text-center text-xl font-medium md:text-3xl py-1 text-yellow-400'>Post your suggestions and complaints</h1>
                     <div className="px-5 md:px-24 py-10 text-gray-400">
                         <form action="mailto:rajankumarcpr002@gmail.com" method="post" encType="text/plain" onSubmit={e => e.preventDefault()}>
                             <div className=''>
                                 <label className='label text-white' htmlFor="name">Name</label>
-                                <input className='input md:w-1/2 :border-slate-900 bg-[#0e1e2c]' value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='Name' required />
+                                <input className='input md:w-1/2 :border-slate-900 bg-[#0e1e2c]' value={profileData.name} readOnly type="text" placeholder='Name' required />
                             </div>
                             <div className=''>
                                 <label className='label text-white' htmlFor="name">Email</label>
-                                <input className='input md:w-1/2 bg-[#0e1e2c]' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} type="email" required />
+                                <input className='input md:w-1/2 bg-[#0e1e2c]' placeholder='Email' value={profileData.email} readOnly type="email" required />
                             </div>
                             <div className=''>
                                 <label className='label text-white' htmlFor="name">Your message</label>
