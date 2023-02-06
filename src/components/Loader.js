9f3f930df8b2729ab74ea6553009ffc7135d87c9
Rajan kumar <krcpr007@gmail.com>
2023-02-06 10:49:27 +0530

    warning removed

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,12 +3,12 @@ import React from 'react'
 function Loader() {
     return (
         <>
-            <div class="flex h-screen justify-center items-center">
+            <div className="flex h-screen justify-center items-center">
                 {/* <!-- Edge X Ring Spinner s1 --> */}
-                <div class="w-12 h-12 rounded-full animate-spin border-x border-solid border-yellow-500 border-t-transparent shadow-md"></div>
+                <div className="w-12 h-12 rounded-full animate-spin border-x border-solid border-yellow-500 border-t-transparent shadow-md"></div>
             </div>
         </>
     )
 }
 
-export default Loader
+export default Loader;
